17e2823013b718beacc5a3c4a500c2671695b5e1
Chaim Bryski <luzybry94@gmail.com>
2020-08-13 23:16:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/\d{10}|\W\d{3}\W\d{3}\W\d{4}|\W\d{3}\W\d{7}/)
+  if phone.scan(/\d{10}|\W\d{3}\W\d{3}\W\d{4}\b|\W\d{3}\W\d{7}/)
     true
   else
     false
